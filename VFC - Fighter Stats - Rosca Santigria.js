a7520a268a418c04f6e044c9147dd4906092ac42
Roscalion <Roscalion@users.noreply.github.com>
2022-03-17 13:33:49 -0600

    Added VFC stats
    Added VFC stats for Rosca Santigria

---
+++ VFC - Fighter Stats - Rosca Santigria.js
@@ -0,0 +1,116 @@
+/* 
+
+After Blodosport Fighting League, Rosca decided to ontinue to to other fighting leagues. His entire goal
+was to win over 20 fights. And so far he had won over 15. He didn't care about the belts, titles, none of that. He just
+needed to get that number. 
+
+*/
+
+
+// PLAYER INFO // 
+
+const playerInfo = {
+    fighterName: "Rosca Santigria",
+    posture: "Nate Diaz",
+    personality: "GeorgeS St-Pierre",
+
+}
+
+
+// PLAYER ATTRIBUTES // 
+
+const playerAttributes = {
+    striking: 68,
+    grappling: 77,
+    stamina: 77,
+    health: 81,
+}
+
+// STRIKING STATS //
+
+const strikingStats = {
+    power: 81,
+    strikeSpeed: 60,
+    accuracy: 64, 
+    blocking: 65,
+    headMovement: 70,
+    footwork: 64,
+    switchStance: 60,
+}
+
+// GRAPPLING STATS //
+
+const grapplingStats = {
+    clinchThrows: 64,
+    clinchGrapple: 64,
+    takeDown: 90,
+    takedownDefense: 66,
+    
+    topGame: 80,
+    bottomGame: 90,
+    submissionOffense: 75,
+    submissionDefense: 90,
+     
+}
+
+// FITNESS STATS // 
+
+const fitnessStats = {
+    strikingStamina: 71,
+    grappleStamina: 91,
+    endurnace: 71,    
+}
+
+// HEALTH STATS // 
+
+const healthStats = {
+    chinStrength: 100,
+    bodyStrength: 74,
+    legStrength: 77,
+    toughness: 71,
+    heart: 83,
+}
+
+
+// MOVES // 
+
+const moves = {
+    guillotine: "lvl 5/5",
+    rearNakedChoke: "lvl 5/5",
+ 
+    coreGroundMoves: "lvl 5/5",
+    boxingCombos: "lvl 5/5",
+    groundTransitionsWrestling: "lvl 5/5",
+    groundTransitionsBJJ: "lvl 5/5",
+
+    singleLegTakedown: "lvl 5/5",
+    doubleLegTakedown: "lvl 5/5",
+    
+    reversalWrestling: "lvl 5/5",
+    reversalBJJ: "lvl 5/5",
+   
+
+    sweepsBJJ: "lvl 4/5",
+
+    triangle: "lvl 3/5",
+    legKick: "lvl 3/5",
+    
+    hook: "lvl 2/5",
+    upperCut: "lvl 2/5",
+    northSouthChoke: "lvl 2/5",
+  
+    rollingKneebar: "lvl 1/5",
+    gogoplata: "lvl 1/5",
+    bullDogChoke: "lvl 1/5",
+   
+
+
+}
+
+
+// PERKS //
+
+const perks = {
+    escapeArtist: "lvl 3/5",
+    groundAndPound: "lvl 2/5",
+}
